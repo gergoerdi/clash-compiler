0475e54f075acd24786fbbf759027bec203d7efa
Leon Schoorl <leon@qbaylogic.com>
2019-03-12 14:07:20 +0100

    Disable doctests on HEAD

--- .ci/setup.sh
+++ .ci/setup.sh
@@ -14,7 +14,7 @@ ghc --version
 cp .ci/cabal.project.local .
 
 case "$GHC" in
-  ghc-8.6* )
+  ghc-8.6*|ghc-head)
     # see https://github.com/haskell/haddock/issues/900
     echo "Haddock and doctests are broken on GHC 8.6, disabling"
     cp .ci/cabal.project.local-8.6 cabal.project.local
